785fc5acecfb6d98a72aba46336c04163d89394c
rimeeee <rimelamrani@yahoo.fr>
2022-06-21 10:09:48 +0200

    ajout xfenetre et snake rime

---
+++ exos_persos/snake/rime_lamrani/snake.c
@@ -0,0 +1,104 @@
+#include <SDL2/SDL.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char** argv) {
+
+  srand(time(NULL));
+  (void)argc;
+  (void)argv;
+
+  SDL_Window* window = NULL;
+  SDL_Renderer* renderer = NULL;
+  SDL_DisplayMode screen;
+  void end_sdl(char ok,                                               // fin normale : ok = 0 ; anormale ok = 1
+             char const* msg,                                       // message à afficher
+             SDL_Window* window,                                    // fenêtre à fermer
+             SDL_Renderer* renderer) {                              // renderer à fermer
+  char msg_formated[255];                                                         
+  int l;                                                                          
+
+  if (!ok) {                                                        // Affichage de ce qui ne va pas
+    strncpy(msg_formated, msg, 250);                                              
+    l = strlen(msg_formated);                                                     
+    strcpy(msg_formated + l, " : %s\n");                                          
+
+    SDL_Log(msg_formated, SDL_GetError());                                        
+  }                                                                               
+
+  if (renderer != NULL) {                                           // Destruction si nécessaire du renderer
+    SDL_DestroyRenderer(renderer);                                  // Attention : on suppose que les NULL sont maintenus !!
+    renderer = NULL;
+  }
+  if (window != NULL)   {                                           // Destruction si nécessaire de la fenêtre
+    SDL_DestroyWindow(window);                                      // Attention : on suppose que les NULL sont maintenus !!
+    window= NULL;
+  }
+
+  SDL_Quit();                                                                     
+
+  if (!ok) {                                                        // On quitte si cela ne va pas                
+    exit(EXIT_FAILURE);                                                           
+  }                                                                               
+}                        
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) end_sdl(0, "ERROR SDL INIT", window, renderer);
+
+  SDL_GetCurrentDisplayMode(0, &screen);
+
+  /* Création de la fenêtre */
+  window = SDL_CreateWindow("Premier dessin",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen.w * 0.66,
+            screen.h * 0.66,
+            SDL_WINDOW_OPENGL);
+  if (window == NULL) end_sdl(0,"ERROR WINDOW CREATION", window, renderer);
+
+  /* Création du renderer */
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == NULL) end_sdl(0, "ERROR RENDERER CREATION", window, renderer);
+  SDL_Rect rectangle[10]; 
+                       // tableau de rectangle pour en avoir plusieurs                                        
+  for (int i=0;i<10;i++) {      
+    SDL_SetRenderDrawColor(renderer,    // creation de rectangles modelisant un serpent avec des couleurs aleatoires
+                          rand()%255+20,           
+                          rand()%255,       
+                          rand()%255,          
+                          255);                                    
+    rectangle[i].x =  40*i; 
+    rectangle[i].h = 10+rand()%10;                                                 
+    rectangle[i].y =  20-rectangle[i].h/2;                                                  
+    rectangle[i].w = 50;                                               
+     
+    SDL_RenderDrawRect(renderer, &rectangle[i]);
+
+  }
+
+
+  SDL_RenderPresent(renderer);                         
+  SDL_Delay(100); 
+ 
+  while(1) {                                              // boucle infinie pour faire deplacer le serpent 
+    SDL_SetRenderDrawColor(renderer,0, 0 , 0, 255);  
+
+  SDL_RenderClear(renderer);
+                                                              
+  for (int i=0;i<10 ; i++) {      
+      SDL_SetRenderDrawColor(renderer,
+                              rand()%255 +20 ,           
+                              rand()%255  ,       
+                            rand()%255    ,          
+                            255);                                    
+      rectangle[i].x = rectangle[i].x+ 50;                                                  
+                                                        
+      
+      SDL_RenderDrawRect(renderer, &rectangle[i]);
+                            
+  }                                 
+  SDL_RenderPresent(renderer);                         
+  SDL_Delay(100);                                     
+ }
+
+  /* on referme proprement la SDL */
+  end_sdl(1, "Normal ending", window, renderer);
+  return EXIT_FAILURE;
+}        
---
+++ exos_persos/x_fenetre/rime_lamrani/xfen.c
@@ -0,0 +1,128 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+/********************************************/
+/* Vérification de l'installation de la SDL */
+/********************************************/
+void CreateWindow(SDL_Window **tab){
+  int i;
+  int j=1;
+  int x, y;
+  for(i=0;i<=9;i++){
+    tab[i]=NULL;
+    tab[i]=SDL_CreateWindow("Fenêtre",50*i, 50*i, 200, 200,SDL_WINDOW_RESIZABLE);           
+    if (tab[i]==NULL) {
+      SDL_Log("Error : SDL window 1 creation - %s\n",SDL_GetError());                 
+      SDL_Quit();                                    
+      exit(EXIT_FAILURE);
+    } 
+  }
+  SDL_GetWindowPosition(tab[9],&x,&y);
+  for(i=10;i<=18;i++){
+    tab[i]=NULL;
+    tab[i]=SDL_CreateWindow("Fenêtre",450+50*j,450-50*j, 200, 200,SDL_WINDOW_RESIZABLE); 
+    j++;        
+    if (tab[i]==NULL) {
+      SDL_Log("Error : SDL window 1 creation - %s\n",SDL_GetError());                 
+      SDL_Quit();                                    
+      exit(EXIT_FAILURE);
+    } 
+  }    
+}
+void Delete_Window(SDL_Window * tab []){
+  int i;
+  for(i=0;i<=18;i++){
+    SDL_DestroyWindow(tab[i]);
+}
+}
+void Move_WindowLeft(SDL_Window * tab [], int * continuer){
+  int size;
+  int x, y;
+  int i;
+  int window_x, window_y;
+  SDL_DisplayMode mode;
+  SDL_GetCurrentDisplayMode(0, &mode);
+  SDL_GetWindowPosition(tab[18],&x,&y);
+  while (i<=18 && *continuer==0){
+    if(x+250<=mode.w){
+        SDL_GetWindowPosition(tab[i],&window_x, &window_y);
+        SDL_SetWindowPosition(tab[i],window_x+50,window_y);
+        i++;
+    }
+    else{
+      *continuer = 1;
+    }
+  }      
+}
+void Move_WindowRight(SDL_Window * tab [], int * continuer){
+  int size;
+  int x, y;
+  int i=0;
+  int window_x, window_y;
+  SDL_DisplayMode mode;
+  SDL_GetCurrentDisplayMode(0, &mode);
+  SDL_GetWindowPosition(tab[0],&x,&y);
+  while (i<=18 && *continuer==1){
+    if(x-250>=0){
+        SDL_GetWindowPosition(tab[i],&window_x, &window_y);
+        SDL_SetWindowPosition(tab[i],window_x-50,window_y);
+        i++;
+    }
+    else{
+      *continuer = 0;
+    }
+  }      
+}
+
+int main(int argc, char **argv){
+  (void)argc;
+  (void)argv;
+  SDL_Window * tab[18];
+  SDL_bool
+    program_on = SDL_TRUE,                          
+    paused = SDL_FALSE;                             
+  SDL_Event event;
+  int sens= 1;
+  if (SDL_Init(SDL_INIT_VIDEO) != 0){
+    SDL_Log("Error : SDL initialisation - %s\n", SDL_GetError());
+    exit(EXIT_FAILURE);
+  } 
+  CreateWindow(tab);
+  while (program_on) {                              
+  if(SDL_PollEvent(&event)) { 
+    if(program_on==SDL_TRUE){
+      if(sens==0){
+        Move_WindowLeft(tab, &sens);
+      }
+      else{
+        Move_WindowRight(tab, &sens);
+      }
+      SDL_Delay(100);  
+    }                                                     
+    switch (event.type) {                    
+      case SDL_QUIT:                                
+        program_on = SDL_FALSE;                     
+        break;
+      case SDL_KEYDOWN:                                                                         
+        switch (event.key.keysym.sym){             
+        case SDLK_p:                                
+        case SDLK_SPACE:                            
+                                   
+          break;
+        case SDLK_ESCAPE:                           
+        case SDLK_q:                               
+          program_on = SDL_FALSE;                                                               
+          break;
+        default:                                    
+          break;
+        }
+        break;
+      default:                                    
+        break;
+    }
+  }
+  }
+  Delete_Window(tab); 
+  SDL_Quit();     
+  return 0;
+}
