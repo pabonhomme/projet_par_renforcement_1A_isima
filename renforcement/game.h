d84f22275ed39832b602c3ca7b9f882e0fb29851
pabonhomme1 <pabonhomme1@turing.local.isima.fr>
2022-06-28 11:48:58 +0200

    verif teleporteur

--- renforcement/game.h
+++ renforcement/game.h
@@ -8,6 +8,27 @@
 #define SCREEN_WIDTH  600
 #define SCREEN_HEIGHT 600
 
+#define NB_TELEPORTER 20
+
+typedef struct character
+{
+	SDL_Texture* sprite;
+	SDL_Rect position;
+	SDL_Rect state;
+	int row;
+	int column;
+
+} Character_t;
+
+typedef struct teleporter
+{
+	int positionRow;
+	int positionColumn;
+	int destinationRow;
+	int destinationColumn;
+
+} Teleporter_t;
+
 void display_charac(SDL_Texture* character_texture, SDL_Renderer* renderer);
 void teleport_to(SDL_Texture* character_texture, SDL_Renderer* renderer);
 
