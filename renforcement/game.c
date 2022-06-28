a35f9ea70845f3c35562c246f92d89d12f481e3c
MLG <mlegall29asb@gmail.com>
2022-06-28 16:11:55 +0200

    premier test

--- renforcement/game.c
+++ renforcement/game.c
@@ -13,3 +13,26 @@ int hasToTeleport(Teleporter_t tabTeleporter[], Character_t character)
 
 	return hasToTeleport;
 }
+
+void initCharacter(Character_t character, SDL_Texture* sprite)
+{
+	SDL_Rect source = {0};
+	int offset_w, offset_h;
+
+	character.sprite = sprite;
+
+	SDL_QueryTexture(character.sprite,NULL,NULL,&source.w, &source.h);
+    offset_w = source.w/4;
+    offset_h = source.h/4;  
+
+	(character.position).x = 17;
+	(character.position).y = 10;
+	(character.position).w = offset_w * 0.8;
+	(character.position).h = offset_h * 0.8;
+	(character.state).x = 0;
+	(character.state).y = 0;
+	(character.state).w = offset_w;
+	(character.state).h = offset_h;
+	character.row = 1;
+	character.column = 1;
+}
